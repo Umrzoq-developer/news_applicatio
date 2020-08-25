6dedc939f2d673db64a036dcc23d543b4c7d1ee7
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-25 12:43:34 +0500

    added all page

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import NewsListItem from "../NewsListItem/NewsListItem";
-import {Skeleton} from 'antd';
+import {Pagination, Skeleton} from 'antd';
 //styling
-import {NewsCol, NewsRow} from "./NewsListStyle";
+import {NewsCol, NewsRow, PaginationDiv} from "./NewsListStyle";
 
 const NewsList = ({news, loading}) => {
+    const [minValue, setMinValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(12);
+
+    const handlePageClick = (value) => {
+        setMinValue((value-1)*12);
+        setMaxValue(value*12);
+    };
+
     return (
         <NewsRow gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            {news.length > 0 ?
-                (news.map(item => {
+            {(news.length > 0 || loading) ?
+                (news.slice(minValue, maxValue).map(item => {
                         return (
                             <NewsCol className='gutter-row' key={item.id} span={5} offset={1}>
-                                <NewsListItem {...item} loading={loading}/>
+                                <NewsListItem {...item}/>
                             </NewsCol>
                         )
                     }
@@ -23,6 +31,16 @@ const NewsList = ({news, loading}) => {
                     </>
                 )
             }
+            <PaginationDiv>
+                <Pagination
+                    pageSize={12}
+                    defaultCurrent={1}
+                    defaultPageSize={12}
+                    onChange={handlePageClick}
+                    total={news.length}
+                />
+            </PaginationDiv>
+
         </NewsRow>
     )
 };
--- src/components/NewsList/NewsListStyle.js
+++ src/components/NewsList/NewsListStyle.js
@@ -9,3 +9,10 @@ export const NewsCol = styled(Col)`
   margin-bottom: 30px;
   box-sizing: border-box;
 `;
+
+export const PaginationDiv = styled.div`
+  padding: 0;
+  margin-left: 55%;
+  margin-top: 2%;
+  margin-bottom: 5%;
+`;
